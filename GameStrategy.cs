88cd5cc04a6aebe0ff8ef80c8de187ff87e475d6
iPilot <ipilot93@gmail.com>
2017-03-31 17:55:47 +0400

    GameStrategyMap

--- GameStrategy.cs
+++ GameStrategy.cs
@@ -5,10 +5,47 @@ using HoMM.ClientClasses;
 
 namespace Homm.Client
 {
+	public class StrategyMapInfo
+	{
+		private bool[,] visited;
+		private MapObjectData[,] objects;
+		private readonly int height;
+		private readonly int width;
+
+		public StrategyMapInfo(HommSensorData sensorData)
+		{
+			height = sensorData.Map.Height;
+			width = sensorData.Map.Width;
+			objects = new MapObjectData[height, width];
+			visited = new bool[height, width];
+		}
+
+		public void RefreshMapState(HommSensorData data)
+		{
+			foreach (var obj in data.Map.Objects)
+			{
+				if (objects[obj.Location.X, obj.Location.Y] == null || objects[obj.Location.X, obj.Location.Y] != obj)
+					visited[obj.Location.X, obj.Location.Y] = false;
+				objects[obj.Location.X, obj.Location.Y] = obj;
+			}
+		}
+
+		public Tuple<MapObjectData, bool> this[Location location] => 
+			IsOutside(location) 
+			? null 
+			: Tuple.Create(objects[location.X, location.Y], visited[location.X, location.Y]);
+
+		private bool IsOutside(Location location)
+		{
+			return location.X < 0 || location.X >= width || location.Y < 0 || location.Y >= height;
+		}
+	}
+
 	public class GameStrategy
 	{
 		private HommClient client;
 		private HommSensorData sensorData;
+		private StrategyMapInfo map;
 		private Dictionary<Direction, Direction> directions = new Dictionary<Direction, Direction>
 		{
 			{Direction.LeftUp, Direction.RightDown},
@@ -23,25 +60,28 @@ namespace Homm.Client
 		{
 			this.client = client;
 			sensorData = client.Configurate(ip, port, cVarcTag);
+			map = new StrategyMapInfo(sensorData);
 		}
 
 		public void Execute()
 		{
-			while (!sensorData.IsDead)
-			{
+			//while (!sensorData.IsDead)
+			//{
 				sensorData = CollectResourses();
-			}
+			//}
 		}
 
 		private HommSensorData CollectResourses()
 		{
 			var s = new Stack<Direction>();
-			var location = sensorData.Location.ToLocation();
-			foreach (var direction in directions)
+			foreach (var location in sensorData.Location.ToLocation().Neighborhood)
 			{
-				var obj = location.NeighborAt(direction.Key);
+				var obj = map[location];
+				switch (obj)
+				{
+					
+				}
 			}
-
 
 			throw new NotImplementedException();
 		}
