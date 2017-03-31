fa859afc7e9add09211d0d578394a5bd4e490be5
iPilot <ipilot93@gmail.com>
2017-03-31 22:50:14 +0400

    Cell checking fixes

--- GameStrategy.cs
+++ GameStrategy.cs
@@ -11,6 +11,7 @@ namespace Homm.Client
 		private MapObjectData[,] objects;
 		private readonly int height;
 		private readonly int width;
+		private readonly string mySide;
 		public static readonly Tuple<MapObjectData, bool> visitedCell = new Tuple<MapObjectData, bool>(null, true);
 
 		public StrategyMapInfo(HommSensorData sensorData)
@@ -20,6 +21,7 @@ namespace Homm.Client
 			objects = new MapObjectData[height, width];
 			visited = new bool[height, width];
 			RefreshMapState(sensorData);
+			mySide = sensorData.MyRespawnSide;
 		}
 
 		public void RefreshMapState(HommSensorData data)
@@ -32,20 +34,33 @@ namespace Homm.Client
 			}
 		}
 
-		public Tuple<MapObjectData, bool> this[Location location]
+		public Tuple<MapObjectData, bool> this[Location l]
 		{
-			get
-			{
-				return IsOutside(location)
-					? null
-					: Tuple.Create(objects[location.Y, location.X], visited[location.Y, location.X]);
-			}
-			set { visited[location.Y, location.X] = value.Item2; }
+			get { return IsValidMove(l) ? Tuple.Create(objects[l.Y, l.X], visited[l.Y, l.X]) : null; }
+			set { visited[l.Y, l.X] = value.Item2; }
 		}
 
-		private bool IsOutside(Location location)
+		private bool IsOutside(Location l)
 		{
-			return location.X < 0 || location.X >= width || location.Y < 0 || location.Y >= height;
+			return l.X < 0 || l.X >= width || l.Y < 0 || l.Y >= height;
+		}
+
+		private bool IsSafetyObject(Location l)
+		{
+			var obj = objects[l.Y, l.X];
+			return obj == null || obj.Dwelling == null && obj.NeutralArmy == null && obj.Wall == null &&
+				   (obj.Garrison == null || obj.Garrison.Owner == mySide);
+		}
+
+		private bool IsOpponentRespawn(Location l)
+		{
+			return mySide == "Left" && l.X == width - 1 && l.Y == height - 1 ||
+				   mySide == "Right" && l.X == 0 && l.Y == 0;
+		}
+
+		private bool IsValidMove(Location l)
+		{
+			return !IsOutside(l) && !IsOpponentRespawn(l) && IsSafetyObject(l);
 		}
 
 		public List<MapObjectData> FindWeakestEnemy(HommSensorData sensor)
@@ -98,7 +113,8 @@ namespace Homm.Client
 		public GameStrategy(HommClient client, string ip, int port, Guid cVarcTag)
 		{
 			this.client = client;
-			sensorData = client.Configurate(ip, port, cVarcTag, debugMap:true, spectacularView:false);
+			//TODO: FIX PARAMETRES
+			sensorData = client.Configurate(ip, port, cVarcTag, debugMap:true, spectacularView:false,speedUp:true);  
 			map = new StrategyMapInfo(sensorData);
 		}
 
@@ -113,18 +129,12 @@ namespace Homm.Client
 			foreach (var direction in directions)
 			{
 				var obj = map[location.NeighborAt(direction.Key)];
-				if (obj == null || obj.Item2 || !IsSafetyObject(obj.Item1)) continue;
+				if (obj == null || obj.Item2) continue;
 				map[location] = StrategyMapInfo.visitedCell;
 				sensorData = client.Move(direction.Key);
 				CollectResourses();
 				sensorData = client.Move(directions[direction.Key]);
 			}
-		}
-
-		private bool IsSafetyObject(MapObjectData obj)
-		{
-			return obj == null || obj.Dwelling == null && obj.NeutralArmy == null && obj.Wall == null &&
-			       (obj.Garrison == null || obj.Garrison.Owner == sensorData.MyRespawnSide);
 		}
 	}
 }
