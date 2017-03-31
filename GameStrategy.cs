f5ec84c0c3eee980a271dd0c510c1f34b83a4551
iPilot <ipilot93@gmail.com>
2017-03-31 12:21:26 +0400

    Добавьте файлы проекта.

---
+++ GameStrategy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HoMM;
+using HoMM.ClientClasses;
+
+namespace Homm.Client
+{
+	public abstract class GameStrategy
+	{
+		protected HommSensorData sensorData;
+
+		protected GameStrategy(HommSensorData currentSensorData)
+		{
+			sensorData = currentSensorData;
+		}
+
+		protected abstract HommSensorData ReleaseStrategy();
+	}
+
+	public class ResourseCollector : GameStrategy
+	{
+		public ResourseCollector(HommSensorData sensorData) : base(sensorData)
+		{
+		}
+
+		protected override HommSensorData ReleaseStrategy()
+		{
+			return sensorData;
+		}
+	}
+}
